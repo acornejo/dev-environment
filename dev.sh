e1473a6433240bb7729428c23ced448b5b7b0cbf
Alex Cornejo <acornejo@gmail.com>
2024-05-25 11:32:05 -0700

    merged utils into dev.sh

---
+++ dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+HUB_REPO="acornejo/dev-environment"
+LABEL="buildme"
+case $1 in
+  run)
+    sudo docker run --rm -p 8080:8080 -v  ${HOME}:/home/coder/mount -it ${LABEL}
+    ;;
+  build)
+    sudo docker build -t ${LABEL} .
+    ;;
+  push)
+    sudo docker tag ${LABEL} ${HUB_REPO}
+    sudo docker push ${HUB_REPO}
+    ;;
+  pull)
+    sudo docker pull ${HUB_REPO}
+    sudo docker tag ${HUB_REPO} ${LABEL}
+    ;;
+  help)
+    echo "Usage: $0 run|build|push|pull"
+    ;;
+  *)
+    echo "Unknown command '$1'"
+    echo "Try running $0 help"
+    ;;
+esac
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo docker run --rm -p 8080:8080 -v  ${HOME}:/home/coder/mount -it buildme
